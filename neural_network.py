f5f8d0ea33084c662621e10863c815a90d7cc8a4
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2017-01-17 15:37:58 -0200

    Criando a primeira rede neural com Keras

---
+++ neural_network.py
@@ -0,0 +1,32 @@
+#Criando a minha primeira rede neural com keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+
+#fixando a seed aleatoria para manter a reprodutibilidade
+seed = 7
+numpy.random.seed(seed)
+
+#carregando o dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+
+#quebrando a entrada em x e a saida em y
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+#criando o modelo
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+
+#compilando o modelo
+model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'])
+
+#ajustando o modelo
+model.fit(X, Y, nb_epoch=150, batch_size=10)
+
+#estimando a performance do modelo
+scores = model.evaluate(X, Y)
+
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
