a1178531850a14372b4c387b8e10b5d0685f09a3
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2017-02-07 09:47:15 -0200

    Rede Neural simples para reconhecimento de Digitos manuais MNIST

--- mnist_solver.py
+++ mnist_solver.py
@@ -1,36 +1,42 @@
+import numpy
 from keras.datasets import mnist
 from keras.utils import np_utils
 from keras.models import Sequential
-from keras.layers import Convolution2D
-#import matplotlib.pyplot as plt
+from keras.layers import Dropout, Dense
+
+
+def baseline_model():
+    model = Sequential()
+    model.add(Dense(num_pixels, input_dim=num_pixels, init='normal', activation='relu'))
+    model.add(Dense(num_classes, init='normal', activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# fixando o seed para reproductibilidade
+seed = 7
+numpy.random.seed(seed)
 
 #Baixando o dataset
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-#plotando as imgens 
-#plt.subplot(221)
-#plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
-#plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
-#plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
-#plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 
-#plt.show()
+#normalizando a entrada de 0-255 para 0-1
+X_train = X_train / 255
+X_test = X_test / 255
 
-#remodelando o dataset para 2 dimensoes
-WIDTH = 28
-HEIGHT = 28
-CHANNELS = 1
-
-X_train = X_train.reshape(X_train.shape[0], CHANNELS, WIDTH, HEIGHT)
-X_test = X_test.reshape(X_test.shape[0], CHANNELS, WIDTH, HEIGHT);
 
 #codificando as saidas
 Y_train = np_utils.to_categorical(Y_train)
 Y_test = np_utils.to_categorical(Y_test)
+num_classes = Y_test.shape[1]
 
-#criando um modelo com ponto de partida
-model = Sequential()
-model.add(Convulational2D(32, 3, 3, border='valid', input_shape=(CHANNELS, WIDTH, HEIGHT), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model = baseline_model()
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=10, batch_size=200, verbose=2)
+
+#Avaiacao final do modelo
+scores = model.evaluate(X_test, Y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
